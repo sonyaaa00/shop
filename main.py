5d996155e792d33b23172afbedd5741c9644bd5a
sonyaaa00 <s.lukyanova0808@mail.ru>
2024-03-16 13:20:04 +0300

    shop!

---
+++ main.py
@@ -0,0 +1,93 @@
+from flask import *
+import mysql.connector
+
+app = Flask(__name__) #инициализация сервера
+app.secret_key = '1581'
+#------------------------function-------------------------
+@app.before_request
+def connection():
+    con = mysql.connector.connect(
+        host = '92.53.96.11',
+        database = 'sch688_etobaza',
+        user = 'sch688_etobaza',
+        password = 'Qwerty123')
+    g.conn = con
+    
+    
+@app.teardown_request
+def close_connection(er):
+    g.conn.close()
+
+def get_user_by_login(login):
+    cursor = g.conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE sUserLogin = %s', (login, ))
+    data=cursor.fetchall()
+    return {'user':data}
+
+def add_user(user):
+    cursor = g.conn.cursor()
+    cursor.execute('INSERT INTO `users`(`sUserName`, `sUserLogin`, `sUserPassword`, `sUserPhone`, `sUserMail`, `iUserStatus`) VALUES (%s,%s,%s,%s,%s,%s)',(user['name'], user['login'], user['paswd'], user['phone'], user['mail'], 0 ))
+    g.conn.commit()
+    data=cursor.lastrowid
+    return {'lastid':data}
+
+#------------------------route-------------------------
+    
+@app.route("/")
+def register():
+    return render_template('main.html')
+
+@app.route("/ajax/registration", methods=['POST'])
+def ajax_rega():
+    req = request.get_json()
+    user = get_user_by_login(req['login'])
+    print(user)
+    if user['user']:
+        user['error'] = 'пользователь с таким login уже есть:( придумай другой login'
+        user['result'] = False
+        return jsonify(user)
+    user = add_user(req)
+    user['result'] = True
+    return jsonify(user)
+
+@app.route("/darkshop")
+def darkshop():
+    if session:
+        return render_template('darkshop.html', session = session)
+    else:
+        return render_template('403.html')
+        
+        
+
+@app.route("/login")
+def auth():
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect('/login')
+
+@app.route("/ajax/login", methods=['POST'])
+def ajax_login():
+    req = request.get_json()
+    user = get_user_by_login(req['login'])
+    if not user['user']:
+        user['error'] = 'Логин или пароль не тот'
+        user['result'] = False
+        return jsonify(user)
+    if req['paswd'] == user['user'][0][4]:
+        user['result'] = True
+        session['id'] = user['user'][0][0]
+        session['name'] = user['user'][0][1]
+        session['surname'] = user['user'][0][2]
+        return jsonify(user)
+    user['error'] = 'Логин или пароль не тот'
+    user['result'] = False
+    return jsonify(user)
+ 
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+
---
+++ static/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+	function validation() {
+		err = 0;
+		$('.form-control').each(function () {
+			if($(this).val() == ''){
+				$(this).addClass('error');
+				err = 1
+			}else{
+				$(this).removeClass('error');
+			}
+		})
+
+		if (window.location=='/'){
+		    if ($('#paswd').val() != $('#paswd2').val() && $('#paswd').val()!=''){
+                err = 1
+                $('#paswd').addClass('error');
+                $('#paswd2').addClass('error');
+		    }else{
+                $('#paswd').removeClass('error');
+                $('#paswd2').removeClass('error');
+            }
+		}
+		if (err){
+			return false
+		}else{
+			return true
+		}
+	}
+	$('#rega_btn').on('click',function(){
+		if(validation()){
+			$.ajax({
+			  method: "POST",
+			  url: "/ajax/registration",
+			  contentType: "application/json",
+			  dataType: 'json',
+			  data: JSON.stringify({ 
+			  	login: $('#login').val(), 
+			  	paswd: $('#paswd').val(),
+			  	name: $('#name').val(),
+			  	surname: $('#surname').val(),
+			  	phone: $('#phone').val(),
+			  	mail: $('#mail').val(),
+			  }),
+			})
+			.done(function(result) {
+			    window.location.href = '/login';
+			});
+		}
+	})
+
+	$('#login_btn').on('click',function(){
+		if(validation()){
+			$.ajax({
+			  method: "POST",
+			  url: "/ajax/login",
+			  contentType: "application/json",
+			  dataType: 'json',
+			  data: JSON.stringify({ 
+			  	login: $('#login').val(), 
+			  	paswd: $('#paswd3').val(),
+			  }),
+			})
+			.done(function(result) {
+			    if(result.result){
+			    	window.location.href = '/darkshop';
+			    }else{
+			    	$('#login').addClass('error');
+			    	$('#paswd').addClass('error');
+			    	$('.invalid-feedback').html(result.error);
+			    }
+			});
+		}
+	})
+})
+
